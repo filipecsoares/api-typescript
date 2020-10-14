37adbd3f0dc19440471b646735ca1e5a3f4fa4ec
Filipe Soares <filipecarlossoares@gmail.com>
2020-10-14 12:25:24 -0300

    add LoadPurchases useCase

--- src/data/usecases/save-purchases/local-save-purchases.ts
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -2,7 +2,10 @@ import { CacheStore } from '@/data/protocols/cache'
 import { SavePurchases } from '@/domain/usecases'
 
 export class LocalSavePurchases implements SavePurchases {
-    constructor(private readonly cacheStore: CacheStore, private readonly timestamp: Date) { }
+    constructor(
+        private readonly cacheStore: CacheStore, 
+        private readonly timestamp: Date
+    ) {}
 
     async save(purchases: Array<SavePurchases.Params>): Promise<void> {
         this.cacheStore.replace('purchases', {
